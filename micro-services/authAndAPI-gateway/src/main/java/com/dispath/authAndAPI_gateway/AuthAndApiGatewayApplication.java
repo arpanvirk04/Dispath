29a16f11254267119b3bddc6b2d848323c04fa9e
Arpandeep Kaur <159736035+arpanvirk04@users.noreply.github.com>
2026-05-26 20:35:30 -0500

    Add files via upload

---
+++ micro-services/authAndAPI-gateway/src/main/java/com/dispath/authAndAPI_gateway/AuthAndApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dispath.authAndAPI_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAndApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAndApiGatewayApplication.class, args);
+	}
+
+}
